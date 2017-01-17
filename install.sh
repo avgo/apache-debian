0df6e8c1b2ddac24a2e1747a5a28a96527dceb4a
Anton Goncharov <gavmail1985@yandex.ru>
2017-01-17 21:11:27 +0300

    Distrib dir. Snapshot. Logs.

--- install.sh
+++ install.sh
@@ -29,11 +29,20 @@ fi
 
 # Каталог куда будут загружен исходный код apache
 #
+distrib_dir="${install_root}/distrib"
+
+# Каталог куда будут скопирован исходный код apache для дальнейших
+# экспериментов
+#
 src_dir="${install_root}/src"
 
 # Каталог куда будет установлен apache2
 #
 prefix_rp="${install_root}/root"
+
+# Логи для процесса сборки/тестирования
+#
+log_dir="${install_root}/log"
 
 # PID-файл для сервера apache2
 #
@@ -54,32 +63,47 @@ build_dep() {
 install() {
 	cd "${install_root}" || return 1
 
-	echo installing ...
-	build_dep &&
-	mkdir -v "${src_dir}" &&
-	cd "${src_dir}" &&
-	apt-get source apache2 || exit 1
+	printf "checking for distrib in '%s' .. " "${distrib_dir}"
 
-	log_dir="${install_root}/log"
+	if test -d "${distrib_dir}"
+	then
+		echo exists
+	else
+		echo no
+		echo creating directory and downloading apache sources...
+		mkdir -v "${distrib_dir}"
+		cd "${distrib_dir}"
+		apt-get source apache2
+	fi
+
+	printf "checking for src_dir in '%s' .. " "${src_dir}"
+
+	if test -d "${src_dir}"
+	then
+		echo exists
+	else
+		echo no
+		echo creating directory and copying apache sources...
+		mkdir -v "${src_dir}"
+		cp -rf "${distrib_dir}/apache2-"* "${src_dir}" &&
+		echo copying complete.
+	fi
+
+	src_dir_ap="$(ls -d "${src_dir}/apache2-"*)"
 
 	test -d "${log_dir}" || mkdir "${log_dir}"
 
-	cd "apache2-"* || exit 1
+	cd "${src_dir_ap}" || exit 1
 
-	# SNAPSHOT before "configure"
-	find > "${log_dir}/snapshot.0.txt"
+	find > "${log_dir}/0.snapshot.txt"
 
-	./configure --prefix="${prefix_rp}" || exit 1
+	./configure --prefix="${prefix_rp}" 2>&1 | tee "${log_dir}/1.configure.log.txt" || exit 1
+	find > "${log_dir}/1.configure.snapshot.txt"
 
-	# SNAPSHOT after "configure"
-	find > "${log_dir}/snapshot.1.configure.txt"
+	make 2>&1 | tee "${log_dir}/2.make.log.txt" || exit 1
+	find > "${log_dir}/2.make.snapshot.txt"
 
-	make || exit 1
-
-	# SNAPSHOT after "make"
-	find > "${log_dir}/snapshot.2.make.txt"
-
-	make install || exit 1
+	make install 2>&1 | tee "${log_dir}/3.make-install.log.txt" || exit 1
 
 	cd "${install_root}"
 	for cf in httpd.conf index.html; do
@@ -90,8 +114,11 @@ install() {
 			-e 's@%INSTALL_DATE%@'"$(date +"%d %B %Y, %T")"'@g' \
 			"${script_dir}/${cf}.in" > "${cf}"
 	done
+
 	touch "mime.types"
+
 	cp "${script_dir}/index.pl.in" "index.pl" && chmod +x "index.pl"
+
 	start
 }
 
@@ -119,6 +146,7 @@ start() {
 status() {
 	if test -f "${apache2_pid}"; then
 		pid=$(cat "${apache2_pid}") &&
+		echo /proc/$pid
 		ls -l /proc/$pid
 	else
 		echo no PID-file
@@ -131,6 +159,7 @@ su_build_dep() {
 
 vars() {
 	eval "$(print_vars '' \
+		distrib_dir  \
 		install_root  \
 		script_rp     \
 		script_dir    \
@@ -153,11 +182,14 @@ else
 		;;
 	--clean)
 		kill_httpd
-		rm -rf	"${src_dir}"   \
+		rm -rf	"${log_dir}"   \
+			"${src_dir}"   \
 			"${prefix_rp}" \
+			"${install_root}/error.log"  \
 			"${install_root}/httpd.conf" \
 			"${install_root}/index.html" \
-			"${install_root}/error.log"
+			"${install_root}/index.pl"   \
+			"${install_root}/mime.types"
 		;;
 	--install)
 		install
